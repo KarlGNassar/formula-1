8ada6973b0b29cfe33af7494ef5f7acbfd9e3232
KarlGNassar <kgnassar01@gmail.com>
2021-11-22 02:05:26 +0200

    dnf cases

--- src/components/PodiumChart.js
+++ src/components/PodiumChart.js
@@ -5,53 +5,11 @@ const PodiumChart = () => {
     series: [
       {
         name: "Lewis Hamilton",
-        data: [
-          1,
-          2,
-          1,
-          1,
-          7,
-          null,
-          2,
-          2,
-          4,
-          1,
-          2,
-          3,
-          2,
-          null,
-          1,
-          5,
-          2,
-          2,
-          1,
-          1,
-        ],
+        data: [1, 2, 1, 1, 7, 10, 2, 2, 4, 1, 2, 3, 2, 10, 1, 5, 2, 2, 1, 1],
       },
       {
         name: "Max Verstappen",
-        data: [
-          2,
-          1,
-          2,
-          2,
-          1,
-          null,
-          1,
-          1,
-          1,
-          null,
-          9,
-          1,
-          1,
-          null,
-          2,
-          2,
-          1,
-          1,
-          2,
-          2,
-        ],
+        data: [2, 1, 2, 2, 1, 10, 1, 1, 1, 10, 9, 1, 1, 10, 2, 2, 1, 1, 2, 2],
       },
     ],
     options: {
@@ -103,6 +61,15 @@ const PodiumChart = () => {
         max: 10,
         title: {
           text: "Podium",
+        },
+        labels: {
+          formatter: function (val) {
+            if (val === 10) {
+              return "DNF";
+            } else {
+              return Math.floor(val);
+            }
+          },
         },
       },
       colors: ["#62b2c9", "#e97486"],
